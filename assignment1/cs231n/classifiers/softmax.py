d5f1ff5fdccae61e1d650e1599264a5f711f3c73
jo1jun <andrew6072@naver.com>
2021-03-19 15:46:54 +0900

    update

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -33,7 +33,26 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    m = X.shape[0]
+    for i in range(m):
+      s = X[i] @ W
+      s_max = np.max(s)
+      e_s = np.exp(s - s_max) # overflow 방지
+      p = e_s[y[i]] / np.sum(e_s)
+      Li = -np.log(p)
+      loss += Li
+
+      ds = np.zeros_like(s)
+      ds[y[i]] = -1 / (np.sum(e_s) * p)
+      ds += e_s[y[i]] / np.square(np.sum(e_s)) / p
+      dW += X[i].T.reshape(-1,1) @ (e_s * ds).reshape(1,-1)
+
+    loss /= m
+    loss += 0.5 * reg * np.sum(np.square(W))
+
+    dW /= m
+    dW += reg * W
+    
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -58,7 +77,23 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    m = X.shape[0]
+
+    s = X @ W
+    s_max = np.max(s, 1).reshape(-1,1)
+    e_s = np.exp(s - s_max)
+    p = e_s[np.arange(m), y] / np.sum(e_s, 1)
+    loss = np.sum(-np.log(p))
+    loss /= m
+    loss += 0.5 * reg * np.sum(np.square(W))
+
+    ds = np.zeros_like(s)
+    ds[np.arange(m), y] -= 1 / (np.sum(e_s, 1) * p)
+    ds += (e_s[np.arange(m), y] / np.square(np.sum(e_s, 1)) / p).reshape(-1,1)
+    dW = X.T @ (e_s * ds)
+
+    dW /= m
+    dW += reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
